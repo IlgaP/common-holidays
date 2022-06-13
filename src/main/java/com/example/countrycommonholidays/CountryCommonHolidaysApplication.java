1fa133ed690e5ad7b1f1c4b70de8ced12209accd
IlgaP <91965275+IlgaP@users.noreply.github.com>
2022-06-13 12:58:55 +0300

    Initial commit

---
+++ src/main/java/com/example/countrycommonholidays/CountryCommonHolidaysApplication.java
@@ -0,0 +1,13 @@
+package com.example.countrycommonholidays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCommonHolidaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountryCommonHolidaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countrycommonholidays/CountryCommonHolidaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countrycommonholidays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCommonHolidaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
